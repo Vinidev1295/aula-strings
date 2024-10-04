979de9b4eeefc26911090ce6b535a3a8860448aa
Vinidev1295 <vinidias1295@gmail.com>
2024-10-04 09:59:26 -0300

    Delete exercicio string 3.js

--- exercicio string 3.js
+++
@@ -1,6 +0,0 @@
-const nome = prompt("Qual seu nome?");
-const corFavorita = prompt("Qual sua cor favorita?")
-const frase = "Meu nome é " + nome + " e minha cor favorita é " + corFavorita
-console.log(frase)  
-//concatenção é quando juntamos varias strings (palavras), para criar uma nova string e utilizamos o sinal de + para isso.
-
