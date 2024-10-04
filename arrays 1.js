a497f4df6aa57e19ed5dd270b2078d33aa90438d
Vinidev1295 <vinidias1295@gmail.com>
2024-10-04 09:59:48 -0300

    Delete arrays 1.js

--- arrays 1.js
+++
@@ -1,4 +0,0 @@
-// Array com as raças de cachorro
-const cachorros = ["Schnauzer", "Poodle", "Border Collie", "Pastor Alemão", "Pitbull"];
-const escolherRaça = prompt("Digite um número de 0 a 4 para escolher uma raça");
-console.log("Você escolheu a raça: " + cachorros[escolherRaça] + ".");
